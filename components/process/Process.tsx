0abe3e57155abbf1f761316045bf280783a88077
maanasmahato4 <maanasmahato456@gmail.com>
2024-05-06 02:06:07 +0545

    update: process section responsive

--- components/process/Process.tsx
+++ components/process/Process.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Process() {
 	return (
 		<section
-			className='relative grid h-screen grid-cols-2 grid-rows-2 items-stretch justify-center gap-8 p-12'
+			className='relative grid grid-cols-1 grid-rows-4 items-stretch justify-center gap-8 p-12 md:h-screen md:grid-cols-2 md:grid-rows-2'
 			id='process'
 		>
 			<div className='flex flex-col gap-y-8 rounded-md p-4 shadow-md shadow-zinc-400'>
-				<h2 className='text-5xl font-bold text-pink-400'>01</h2>
+				<h2 className='text-4xl font-bold text-pink-400 lg:text-5xl'>01</h2>
 				<h3 className='text-2xl font-bold'>Development</h3>
-				<p className='text-md'>
+				<p className='md:text-md text-sm'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non
 					dicta fuga magni suscipit! Quae rem quidem distinctio accusamus
 					mollitia! Deserunt maxime, temporibus in repellat eum eos adipisci
@@ -17,9 +17,9 @@ export default function Process() {
 				</p>
 			</div>
 			<div className='flex flex-col gap-y-8 rounded-md p-4 shadow-md shadow-zinc-400'>
-				<h2 className='text-5xl font-bold text-pink-400'>02</h2>
-				<h3 className='text-2xl font-bold'>Design Projects</h3>
-				<p className='text-md'>
+				<h2 className='text-4xl font-bold text-pink-400 lg:text-5xl'>01</h2>
+				<h3 className='text-2xl font-bold'>Development</h3>
+				<p className='md:text-md text-sm'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non
 					dicta fuga magni suscipit! Quae rem quidem distinctio accusamus
 					mollitia! Deserunt maxime, temporibus in repellat eum eos adipisci
@@ -27,9 +27,9 @@ export default function Process() {
 				</p>
 			</div>
 			<div className='flex flex-col gap-y-8 rounded-md p-4 shadow-md shadow-zinc-400'>
-				<h2 className='text-5xl font-bold text-pink-400'>03</h2>
-				<h3 className='text-2xl font-bold'>Planning</h3>
-				<p className='text-md'>
+				<h2 className='text-4xl font-bold text-pink-400 lg:text-5xl'>01</h2>
+				<h3 className='text-2xl font-bold'>Development</h3>
+				<p className='md:text-md text-sm'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non
 					dicta fuga magni suscipit! Quae rem quidem distinctio accusamus
 					mollitia! Deserunt maxime, temporibus in repellat eum eos adipisci
@@ -37,9 +37,9 @@ export default function Process() {
 				</p>
 			</div>
 			<div className='flex flex-col gap-y-8 rounded-md p-4 shadow-md shadow-zinc-400'>
-				<h2 className='text-5xl font-bold text-pink-400'>04</h2>
-				<h3 className='text-2xl font-bold'>Consultants</h3>
-				<p className='text-md'>
+				<h2 className='text-4xl font-bold text-pink-400 lg:text-5xl'>01</h2>
+				<h3 className='text-2xl font-bold'>Development</h3>
+				<p className='md:text-md text-sm'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non
 					dicta fuga magni suscipit! Quae rem quidem distinctio accusamus
 					mollitia! Deserunt maxime, temporibus in repellat eum eos adipisci
