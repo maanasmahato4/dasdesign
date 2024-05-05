3237dbf0b2c97c38b29ffb3f53fdafdb697dc5b7
maanasmahato4 <maanasmahato456@gmail.com>
2024-05-06 02:02:46 +0545

    update: hero section responsive

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -8,8 +8,8 @@ export default function Hero() {
 			id='hero'
 		>
 			<Image className='-z-10 blur-sm' fill src={heroBg} alt='' />
-			<div className='flex w-[40%] flex-col justify-center gap-y-8'>
-				<h1 className='w-[65%] text-5xl text-white'>
+			<div className='flex w-[80%] flex-col justify-center gap-y-8 sm:w-[40%]'>
+				<h1 className='w-[80%] text-5xl text-white md:w-[65%]'>
 					WE HELP SMALL BUSINESSES AND CORPORATIONS
 				</h1>
 				<p className='rounded-sm bg-yellow-300 p-4'>
