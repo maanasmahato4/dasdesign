431918155cff33c54ea05469ad4993a5e8d2382d
maanasmahato4 <maanasmahato456@gmail.com>
2024-05-06 01:57:25 +0545

    update: nav responsive

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,13 +1,31 @@
 import Link from 'next/link';
+import { Home, Users, CandlestickChart, BookHeart, Phone } from 'lucide-react';
 
 export default function NavBar() {
 	return (
 		<header
-			className='flex w-full flex-row items-center justify-between px-8 py-4'
+			className='flex w-full flex-row items-center justify-center px-8 py-4 md:justify-between'
 			id='nav'
 		>
-			<h3 className='italic'>DasDesign</h3>
-			<nav className='flex flex-row gap-x-4'>
+			<h3 className='hidden italic md:block'>DasDesign</h3>
+			<nav className='flex w-full flex-row justify-evenly sm:hidden'>
+				<Link href='#hero' className='hover:text-cyan-600'>
+					<Home />
+				</Link>
+				<Link href='#about' className='hover:text-cyan-600'>
+					<Users />
+				</Link>
+				<Link href='#process' className='hover:text-cyan-600'>
+					<CandlestickChart />
+				</Link>
+				<Link href='#testimonial' className='hover:text-cyan-600'>
+					<BookHeart />
+				</Link>
+				<Link href='#contact' className='hover:text-cyan-600'>
+					<Phone />
+				</Link>
+			</nav>
+			<nav className='hidden flex-row gap-x-4 sm:flex'>
 				<Link href='#hero' className='hover:text-cyan-600'>
 					Home
 				</Link>
