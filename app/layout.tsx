543344ff565bba3035098277b9fcf61415c8a96a
maanasmahato4 <maanasmahato456@gmail.com>
2024-05-07 05:12:09 +0545

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import NavBar from '@/components/navbar/NavBar';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,8 @@ export const metadata: Metadata = {
 		default: 'The DasDesign',
 		template: '%s - DasDesign',
 	},
-	description: 'We create the best looking websites for your business',
+	description:
+		'We at the dasdesign create the best looking websites for your business',
 };
 
 export default function RootLayout({
@@ -21,6 +23,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<Head>
+				<meta
+					name='google-site-verification'
+					content='Aoqh9TJe1jzE-lGdHdsCz03d6YLQm5R-mOSf74ZDJY0'
+				/>
+			</Head>
 			<body className={inter.className}>
 				<NavBar />
 				<main>{children}</main>
